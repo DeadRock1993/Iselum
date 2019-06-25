b6e29164cd56398ede3e72605e71664c57af44c9
Dilovar <impala7691@gmail.com>
2019-06-25 11:50:18 +0500

    wordpressready

---
+++ header.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <title>Cловари и энциклопедии</title>
+        <meta property="og:title" content="Термины и Словари">
+        <meta name="description" content="Термины и Словари.">
+        <meta property="og:description" content="Термины и Словари. Ваш личный путеводитель в мире энциклопедий и словарей.">
+        <meta name="device-detect" content="desktop">
+        <meta property="og:url" content="">
+        <link href="./index.html" rel="canonical">
+        <link rel="shortcut icon" href="<?php echo get_template_directory_uri() ?>/favicon.ico" type="image/x-icon">
+        <link href="<?php echo get_template_directory_uri() ?>/css/bootstrap.min.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri() ?>/style.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri() ?>/css/mobile.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri() ?>/css/jquery-ui.css" rel="stylesheet">
+        <link href="<?php echo get_template_directory_uri() ?>/css/typeaheadjs.css" rel="stylesheet">
+        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <meta property="og:site_name" content="https:site.tj">
+        <meta property="og:type" content="article">
+        <meta property="og:locale" content="ru">
+        <meta property="og:image:width" content="1200">
+        <meta property="og:image:height" content="630">
+        <meta name="apple-mobile-web-app-title" content="Словари">
+
+    </head>
+
+    <body id="top">
+    
+        <header class="header hidden-print">
+            <nav>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-auto">
+                            <a href="./index.html" title="Cловари и энциклопедии"><i class="logo"></i></a>
+                        </div>
+                        <div class="col">
+                            <div class="header-search-form">
+                                <!--Задать ACTION для фукционала поиска-->
+                                <form action="" method="get">
+                                    <div class="input-group">
+                                        <span class="header-search-field">
+                                            <input type="text" class="form-control-lg" name="term" value="" id="search-word" data-provide="typeahead" data-toggle="tooltip" data-placement="bottom" data-trigger="manual" title="Запрос должен быть длиннее 2 символов...">
+                                        </span>
+                                        <span class="input-group-btn">
+                                            <button class="btn btn-secondary btn-lg" type="submit" id="search-submit" style="cursor: pointer;" title="Найти">
+                                                <i class="fa fa-search" aria-hidden="true"></i>
+                                            </button>
+                                        </span>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
---
+++ index.php
@@ -0,0 +1,124 @@
+<?php get_header(); ?>
+<div class="container">
+    <section id="dictionary-index">
+        <h2>Термины</h2>
+        
+        <div class="row"><!--Раздел термины-->
+        
+        <!-- Три колонки div=col-md-4  разделяют на три части термины-->
+                    <div class="col-md-4">
+                        <h3>Бизнес, экономика и юриспруденция</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/accounting">Большой бухгалтерский словарь</a></li>
+                            <li><a href="https://gufo.me/dict/law">Большой юридический словарь</a></li>
+                            <li><a href="https://gufo.me/dict/financial_terms">Финансовый словарь терминов</a></li>
+                            <li><a href="https://gufo.me/dict/economics_terms">Экономический словарь терминов</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Математика и физика</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/mathematics_encyclopedia">Математическая энциклопедия</a></li>
+                            <li><a href="https://gufo.me/dict/physics">Физический энциклопедический словарь</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Строительство и геология</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/architecture">Архитектурный словарь</a></li>
+                            <li><a href="https://gufo.me/dict/mining_encyclopedia">Горная энциклопедия</a></li>
+                            <li><a href="https://gufo.me/dict/building_terms">Строительная терминология</a></li>
+                            <li><a href="https://gufo.me/dict/ecology_terms">Экологические термины и определения</a></li>
+                        </ul>
+                    </div>
+
+
+        </div>
+
+            
+        <h2>Словари</h2>
+        
+        <div class="row"><!--Раздел словари-->
+
+        <!-- Три колонки div=col-md-4  разделяют на три части словари-->
+                    <div class="col-md-4">
+                        <h3>Английский</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/enru_medical">Англо-русский медицинский словарь</a></li>
+                            <li><a href="https://gufo.me/dict/enru_marine">Англо-русский морской словарь</a></li>
+                            <li><a href="https://gufo.me/dict/enru_muller">Англо-русский словарь Мюллера</a></li>
+                            <li><a href="https://gufo.me/dict/enru_printing">Англо-русский словарь по полиграфии</a></li>
+                            <li><a href="https://gufo.me/dict/enru_economics">Англо-русский словарь по экономике</a></li>   
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Болгарский</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/bgru">Болгарско-русский словарь</a></li>
+                            <li><a href="https://gufo.me/dict/rubg">Русско-болгарский словарь</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-4">
+                        <h3>Азербайджанский</h3>
+                        <ul class="list-unstyled">
+                            <li><a href="https://gufo.me/dict/azru">Азербайджанско-русский словарь</a></li>
+                            <li><a href="https://gufo.me/dict/ruaz">Русско-азербайджанский словарь</a></li>
+                        </ul>
+                    </div>
+            </div>
+</section>
+</div>
+
+        <br><br><br>
+
+        <div class="container adverterans text-center">
+            <style>
+                .adverterans {
+                    max-width: 970px;
+                }
+
+                .adverterans, .adverterans a {
+                    color: #555;
+                }
+
+                .adverterans ins:before {
+                    content: "\201C";
+                    font-size: 300%;
+                    color: #ccc;
+                    display: block;
+                    margin-bottom: 15px;
+                }
+
+                .adverterans ins {
+                    font-size: 150%;
+                    line-height: 110%;
+                    font-family: "Times New Roman";
+                }
+
+                .adverterans ins a {
+                    text-decoration: none;
+                }
+
+                .adverterans .lec {
+                    margin-top: 15px;
+                }
+
+                .adverterans .lec:after {
+                    content: "— Станислав Ежи Лец —";
+                    font-size: 80%;
+                    display: block;
+                    margin-top: 10px;
+                    color: #888;
+                }
+            </style>
+            <ins data-revive-zoneid="12" data-revive-id="36e20d1b6d979a4dc88d48fb0f2d4032"></ins>
+            <script async src="https://ad.adverterans.ru/delivery/asyncjs.php"></script>
+            <div class="lec">
+                <img src="https://cdn.gufo.me/i/lec-photo.jpg" class="rounded-circle lec" width="50" height="50">
+            </div>
+        </div>
+
+        <br>
+        
+<?php get_footer();?>
+
