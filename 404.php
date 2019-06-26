5bd1bb78368ddf2346c854877400e46b56ac20bb
Dilovar <impala7691@gmail.com>
2019-06-26 17:13:44 +0500

    улучшил 404 стр добавив картинку

--- 404.php
+++ 404.php
@@ -1,9 +1,9 @@
 <?php get_header(); ?>
 
     <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2 class="text-center">404</h2>
+        <div class="row justify-content-center">
+            <div class="col col-md-5">
+                <img class="img-fluid" src="<?php echo get_template_directory_uri() ?>/img/404.jpg" alt="Нет такой страницы.">
                 <h2 class="text-center">Такой страницы не существует.</h2>
             </div>
         </div>
